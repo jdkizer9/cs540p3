a6dadb0013d302101f0fecde08f4a72bdc2575b6
James <james.kizer@gmail.com>
2013-05-11 11:39:03 -0400

    Additional development work on Array<> Template. Working implementation of default constructor, index operator, and same type copy constructor.

--- Array.hpp
+++ Array.hpp
@@ -13,26 +13,141 @@
 
 namespace cs540 {
     
-    template<typename T, size_t len, size_t ... args>
-    struct ArrayTypeHelper {
-        typedef typename ArrayTypeHelper<T, args...>::array_t array_t[len];
+    template <typename T, size_t... dims>
+    class Array {
+    public:
+        
+        //Array Helper Classes
+        template<typename U, size_t len, size_t ... args>
+        struct ArrayHelper {
+            
+            static_assert(len>0, "Dimension Out of Range: must be positive");
+            
+            //typedef typename ArrayTypeHelper<U, args...>::array_t U_A;
+            //need to define entry type for [] return type
+            using entry_t = ArrayHelper<U, args...>;
+            
+            //U_A a_[len];
+            ArrayHelper<U, args...> a_[len];
+            size_t length;
+            
+            //default constructor
+            ArrayHelper () : length(len){};
+            
+            //assignment operator
+            ArrayHelper &operator=(const ArrayHelper &other) {
+                
+                //if length != other.length, throw
+                for (int i=0; i<len; i++)
+                    a_[i] = other.a_[i];
+                
+                return *this;
+            }
+            
+            //assignment operator
+            template <typename V>
+            ArrayHelper &operator=(const ArrayHelper<V, len, args...> &other) {
+                
+                //if length != other.length, throw
+                for (int i=0; i<len; i++)
+                    a_[i] = other.a_[i];
+                
+                return *this;
+            }
+            
+            //copy constructor
+//            ArrayHelper (const ArrayHelper &other) : length(len){
+//            
+//            }
+            
+            entry_t &operator[](const size_t i) {
+                //throw
+                //if(i<0 || i<=len)
+                return a_[i];
+            }
+            
+        };
+        
+        template<typename U, size_t len>
+        struct ArrayHelper<U, len> {
+            
+            static_assert(len>0, "Dimension Out of Range: must be positive");
+            
+            using entry_t = U;
+            U a_[len];
+            size_t length;
+            
+            //default constructor
+            ArrayHelper () : length(len) {};
+            
+            //assignment operator
+            ArrayHelper &operator=(const ArrayHelper &other) {
+                
+                //if length != other.length, throw
+                for (int i=0; i<len; i++)
+                    a_[i] = other.a_[i];
+                
+                 return *this;
+            }
+            
+            template <typename V>
+            ArrayHelper &operator=(const ArrayHelper<V, len> &other) {
+                
+                //if length != other.length, throw
+                for (int i=0; i<len; i++)
+                    //setting type U to type V
+                    a_[i] = other.a_[i];
+                
+                return *this;
+            }
+            
+            //copy constructor
+//            ArrayHelper (const ArrayHelper &other) : length(len) {
+//                
+//            }
+            
+            U &operator[](const size_t i) {
+                //throw
+                //if(i<0 || i<=len)
+                return a_[i];
+            }
+        };
+        
+        //default constructor
+        Array () {};
+        
+        //copy constructor
+        Array (const Array &other) {
+            
+            //this should kick off the assignment operators in
+            //the helper class
+            a_ = other.a_;
+            
+        }
+        
+        template <typename U>
+        Array (const Array<U, dims...> &other) {
+            
+            //this will only be invoked if dimensions are identical
+            
+            //this should kick off the assignment operators in
+            //the helper class
+            a_ = other.a_;
+            
+        }
+        
+        typedef T ValueType;
+        typedef typename ArrayHelper<T, dims...>::entry_t EntryType;
+        
+        //a_ stores the array in a recursive helper class
+        ArrayHelper<T, dims...> a_;
+        
+        EntryType &operator[](const size_t i) {
+            return a_[i];
+        }
+        
         
     };
-    
-    template<typename T, size_t len>
-    struct ArrayTypeHelper<T, len> {
-        using array_t = T[len];
-    };
-    
-//    template <typename T, size_t... dims>
-//    class Array {
-//        typedef T ValueType;
-//        
-//        
-//        //save dims in a tuple for quick run time comparison
-//        
-//        
-//    };
     
 }
 
--- ArrayTest.cpp
+++ ArrayTest.cpp
@@ -9,25 +9,61 @@
 #include <iostream>
 #include "Array.hpp"
 
-typedef typename cs540::ArrayTypeHelper<double, 2>::array_t ARR;
-typedef ARR ARR2[2];
+
 
 
 int main() {
-    typename cs540::ArrayTypeHelper<double, 2>::array_t array1;
     
-    typename cs540::ArrayTypeHelper<int, 2, 2>::array_t array2;
+    cs540::Array<int, 1> a, b;
     
-    typename cs540::ArrayTypeHelper<int, 2, 2, 2>::array_t array3;
+    a[0] = 1;
+    b[0] = a[0];
     
-    ARR2 i;
+    cs540::Array<short, 1> c = a;
     
     
-    //typename CS540::ArrayTypeHelper<int, 2, 2, 2>::array_t array3;
     
-    array1[0] = 0;
-    array1[1] = 1;
+    //cs540::Array<int, 1, 1> a1, b1;
+   
+    //a1[0][0] = 1;
+    //a1[0][0] = 2;
+    //b1[0][0] = a1[0][0];
     
-    size_t len = sizeof(array3);
+    // Define a 2X3X4 array of integers.  Elements are uninitialized.
+    //cs540::Array<int, 2, 3, 4> a, b;
+    //cs540::Array<short, 2, 3, 4> c;
+    //cs540::Array<short, 1, 3, 4> d;
+    //cs540::Array<int, 0> e1; // This line must cause a compile-time error.
+    //cs540::Array<int, 1, 0, 1> e2; // This line must cause a compile-time error.
+    //cs540::Array<int, 0, 1, 1> e3; // This line must cause a compile-time error.
+    
+//    // Range-checked indexing.
+    //a[0][0][0] = 1234;
+    //a[1][1][1] = a[0][0][0];
+    //a[0][2][3] = 5678; // Set the element in the first plane, 3rd row, and 4th column.
+//    a[0][3][0] = 1; // Out of range, throws.
+//    
+    
+    //cs540::Array<int, 2, 3, 4> a2 = a;
+    //cs540::Array<short, 2, 3, 4> c2 = a;
+    //a = a; // Self-assignment must be a no-op.
+    //b = a; // Okay, as long as dimensions and type match exactly.
+//    a = c; // Member template constructor.
+//    
+//    const Array<int, 2, 3, 4> &const_ref(a);
+//    
+//    int i = const_ref[0][0][0]; // Okay.
+//    const_ref[0][0][0] = 1; // Syntax error.
+//    
+//    // Iterate through in plane major order.  (Note: Normally, you would use
+//    // auto to simplify.)
+//    for (Array<3, int>::FirstDimensionMajorIterator it = a.fmbegin(); it != a.fmend(); ++it) {
+//        cout << *it << endl;
+//    }
+//    
+//    // Iterate through in column major order.
+//    for (Array<3, int>::LastDimensionMajorIterator it = a.lmbegin(); it != a.lmend(); ++it) {
+//        cout << *it << endl;
+//    }
 
 }
