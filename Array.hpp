65f4562d85cfef2e2938d6a5b35eba33589de80f
James <james.kizer@gmail.com>
2013-05-13 21:50:00 -0400

    Working Array Class Template.
    TODO:
    	Self-Assignment in assignment operator must be no-op
    	Out of Range exception in index operator.

--- Array.hpp
+++ Array.hpp
@@ -10,17 +10,195 @@
 #define __CS540project3__Array__
 
 #include <iostream>
+#include <assert.h>
 
 namespace cs540 {
     
     template <typename T, size_t... dims>
     class Array {
     public:
+        typedef T ValueType;
+        static const size_t numDims;
+        static const size_t maxDims[Array::numDims];
         
+    public:
+        class Iterator : public std::iterator<std::forward_iterator_tag, T>
+        {
+        public:
+            
+            Iterator(const Iterator &it) {
+                a = it.a;
+                for(size_t i=0; i<Array::numDims; i++){
+                    d[i] = it.d[i];
+                }
+                endFlag = it.endFlag;
+            }
+            
+            Iterator(const Array *a_) : d{0}, a(const_cast<Array*>(a_)), endFlag(false) {}
+            
+            
+            Iterator &operator=(const Iterator &it) {
+                a = it.a;
+                for(size_t i=0; i<Array::numDims; i++){
+                    d[i] = it.d[i];
+                }
+                endFlag = it.endFlag;
+                return *this;
+            }
+            
+            
+            
+            
+            //equality operators
+            bool operator==(const Iterator &i2) {
+                
+                //check to see if they point to the same array
+                if (a != i2.a)
+                    return false;
+                
+                //check for the end
+                if (endFlag && i2.endFlag)
+                    return true;
+                
+                for(size_t i=0; i<Array::numDims; i++){
+                    if(d[i] != i2.d[i])
+                        return false;
+                }
+                return true;
+            }
+            
+            bool operator!=(const Iterator &i2) {
+                return !( *this == i2);
+            }
+            
+            
+            //need to use the dimensions stored in an array
+            //to access the item
+            Array::ValueType &operator*() {
+                assert(a != nullptr);
+                return a->get(d);
+            }
+                       
+        protected:
+        
+            //0 is most significant
+            //last dim is least significant
+            //last dim = Array::numDims-1
+            //for First Dimension, we actually access last dim first
+            size_t d[Array::numDims];
+            Array *a;
+            bool endFlag;
+        private:
+            virtual void increment() = 0;
+            
+        };
+        class FirstDimensionMajorIterator : public Iterator
+        {
+        public:
+            
+            FirstDimensionMajorIterator() : Iterator(nullptr) {};
+            
+            FirstDimensionMajorIterator(const Array *a_, bool end=false) : Iterator(a_) {
+                if(end)
+                {
+                    for(size_t i=0; i<Array::numDims; i++) {
+                        this->d[i] = maxDims[i]-1;
+                    }
+                    this->d[Array::numDims-1]++;
+                    this->endFlag = true;
+                }
+            }
+            
+            FirstDimensionMajorIterator &operator++() {
+                increment();
+                return *this;
+            }
+            
+            FirstDimensionMajorIterator operator++(int) {
+                auto tmp = *this;
+                increment();
+                return tmp;
+            }
+        private:
+            void increment() {
+                size_t i;
+                for(i=0; i<Array::numDims; i++) {
+                    assert(maxDims[i] > this->d[i]);
+                    
+                    size_t index = Array::numDims-(i+1);
+                    
+                    this->d[index]++;
+                    //if this dimension has not reached max, increment over
+                    if (this->d[index] < maxDims[index])
+                        return;
+                    //otherwise, this dimension is maxed out, set to 0 and increment next dimension
+                    else
+                        this->d[index]=0;
+                    
+                }
+                this->d[Array::numDims-1]++;
+                this->endFlag = true;
+            }
+            
+        };
+        
+        class LastDimensionMajorIterator : public Iterator
+        {
+        public:
+            
+            LastDimensionMajorIterator() : Iterator(nullptr) {};
+            
+            LastDimensionMajorIterator(const Array *a_, bool end=false) : Iterator(a_) {
+                if(end)
+                {
+                    for(size_t i=0; i<Array::numDims; i++) {
+                        this->d[i] = maxDims[i]-1;
+                    }
+                    this->d[0]++;
+                    this->endFlag = true;
+                }
+            }
+            
+            LastDimensionMajorIterator &operator++() {
+                increment();
+                return *this;
+            }
+            
+            LastDimensionMajorIterator operator++(int) {
+                auto tmp = *this;
+                increment();
+                return tmp;                
+            }
+        private:
+            void increment() {
+                size_t i;
+                for(i=0; i<Array::numDims; i++) {
+                    assert(maxDims[i] > this->d[i]);
+                    
+                    this->d[i]++;
+                    //if this dimension has not reached max, increment over
+                    if (this->d[i] < maxDims[i])
+                        return;
+                    //otherwise, this dimension is maxed out, set to 0 and increment next dimension
+                    else
+                        this->d[i]=0;
+                    
+                }
+                this->d[0]++;
+                this->endFlag = true;
+                
+                //assert(i<Array::numDims);
+            }
+            
+        };
+        
+        
+    private:
         //Array Helper Classes
         template<typename U, size_t len, size_t ... args>
-        struct ArrayHelper {
+        class ArrayHelper {
             
+        public:
             static_assert(len>0, "Dimension Out of Range: must be positive");
             
             //typedef typename ArrayTypeHelper<U, args...>::array_t U_A;
@@ -29,10 +207,11 @@ namespace cs540 {
             
             //U_A a_[len];
             ArrayHelper<U, args...> a_[len];
-            size_t length;
+            const size_t length;
+            const size_t level;
             
             //default constructor
-            ArrayHelper () : length(len){};
+            ArrayHelper () : length(len), level(a_[0].level+1) {};
             
             //assignment operator
             ArrayHelper &operator=(const ArrayHelper &other) {
@@ -55,30 +234,42 @@ namespace cs540 {
                 return *this;
             }
             
-            //copy constructor
-//            ArrayHelper (const ArrayHelper &other) : length(len){
-//            
-//            }
-            
             entry_t &operator[](const size_t i) {
                 //throw
                 //if(i<0 || i<=len)
                 return a_[i];
             }
             
+            const entry_t &operator[](const size_t i) const {
+                //throw
+                //if(i<0 || i<=len)
+                return a_[i];
+            }
+            
+            //top level must access entry 0
+            //next level must access entry 1
+            //last level must access Array::numDims - 1 
+            T &get(const size_t d[Array::numDims]) {
+                return a_[ d[(Array::numDims-1)-level] ].get(d);
+            }
+            
         };
         
+        //Base ArrayHelper Class
         template<typename U, size_t len>
-        struct ArrayHelper<U, len> {
+        class ArrayHelper<U, len> {
+            
+            public:
             
             static_assert(len>0, "Dimension Out of Range: must be positive");
             
             using entry_t = U;
             U a_[len];
-            size_t length;
+            const size_t length;
+            const size_t level;
             
             //default constructor
-            ArrayHelper () : length(len) {};
+            ArrayHelper () : length(len), level(0) {};
             
             //assignment operator
             ArrayHelper &operator=(const ArrayHelper &other) {
@@ -100,56 +291,153 @@ namespace cs540 {
                 
                 return *this;
             }
-            
-            //copy constructor
-//            ArrayHelper (const ArrayHelper &other) : length(len) {
-//                
-//            }
-            
+
             U &operator[](const size_t i) {
                 //throw
                 //if(i<0 || i<=len)
                 return a_[i];
             }
+            
+            const U &operator[](const size_t i) const {
+                //throw
+                //if(i<0 || i<=len)
+                return a_[i];
+            }
+            
+            //for base, level will be 0. However, this must access the
+            //last element in d
+            U &get(const size_t d[Array::numDims]) {
+                return a_[ d[(Array::numDims-1)-level] ];
+            }
         };
-        
+    public:
         //default constructor
-        Array () {};
+        Array ()  {};
         
         //copy constructor
-        Array (const Array &other) {
-            
-            //this should kick off the assignment operators in
-            //the helper class
-            a_ = other.a_;
-            
-        }
-        
         template <typename U>
         Array (const Array<U, dims...> &other) {
             
             //this will only be invoked if dimensions are identical
             
-            //this should kick off the assignment operators in
-            //the helper class
-            a_ = other.a_;
+            //reimplement using iterators
+            //we know they are the same dimenstions
+            auto i1b = this->fmbegin();
+            auto i1e = this->fmend();
+            auto i2b = other.fmbegin();
+            for(;i1b != i1e;) {
+                *i1b = *i2b;
+                ++i1b;
+                ++i2b;
+            }
             
         }
         
-        typedef T ValueType;
-        typedef typename ArrayHelper<T, dims...>::entry_t EntryType;
+        Array (const Array &other)  {
+            
+            if(&other == this)
+                return;
+            
+            //reimplement using iterators
+            //we know they are the same dimenstions
+            
+            auto i1b = this->fmbegin();
+            auto i1e = this->fmend();
+            auto i2b = other.fmbegin();
+            for(;i1b != i1e;) {
+                *i1b = *i2b;
+                ++i1b;
+                ++i2b;
+            }
+            
+        }
         
+        //TODO - self assignment must be a no-op
+        Array &operator=(const Array &other) {
+            
+            if(&other == this)
+                return *this;
+            
+            //reimplement using iterators
+            //we know they are the same dimenstions
+            
+            auto i1b = this->fmbegin();
+            auto i1e = this->fmend();
+            auto i2b = other.fmbegin();
+            for(;i1b != i1e;) {
+                *i1b = *i2b;
+                ++i1b;
+                ++i2b;
+            }
+            return *this;
+        }
+        
+        //TODO - self assignment must be a no-op
+        template <typename U>
+        Array &operator=(const Array<U, dims...> &other) {
+            
+            //reimplement using iterators
+            //we know they are the same dimenstions
+            
+            auto i1b = this->fmbegin();
+            auto i1e = this->fmend();
+            auto i2b = other.fmbegin();
+            for(;i1b != i1e;) {
+                *i1b = *i2b;
+                ++i1b;
+                ++i2b;
+            }
+            return *this;
+        }
+
+    private:
+        typedef typename ArrayHelper<T, dims...>::entry_t EntryType;
+        typedef const typename ArrayHelper<T, dims...>::entry_t ConstEntryType;
+
         //a_ stores the array in a recursive helper class
         ArrayHelper<T, dims...> a_;
         
+        T &get(const size_t d[Array::numDims]) {
+            return a_.get(d);
+        }
+        
+    public:
         EntryType &operator[](const size_t i) {
             return a_[i];
         }
         
+        const EntryType &operator[](const size_t i) const {
+            return a_[i];
+        }
+    
+        FirstDimensionMajorIterator fmbegin() const {
+            //Array *myArray = this;
+            return FirstDimensionMajorIterator(this);
+        }
+        
+        FirstDimensionMajorIterator fmend() const {
+            return FirstDimensionMajorIterator(this, true);
+        }
+        
+        LastDimensionMajorIterator lmbegin() const {
+            //Array *myArray = this;
+            return LastDimensionMajorIterator(this);
+        }
+        
+        LastDimensionMajorIterator lmend() const {
+            return LastDimensionMajorIterator(this, true);
+        }
+
         
     };
     
 }
+
+template <typename T, size_t... dims>
+const size_t cs540::Array<T, dims...>::numDims = sizeof...(dims);
+
+template <typename T, size_t... dims>
+const size_t cs540::Array<T, dims...>::maxDims[Array::numDims] = {dims...};
 
 
 #endif /* defined(__CS540project3__Array__) */
--- ArrayTest.cpp
+++ ArrayTest.cpp
@@ -14,56 +14,102 @@
 
 int main() {
     
-    cs540::Array<int, 1> a, b;
+//    cs540::Array<int, 1> a, b;
+//    
+//    a[0] = 1;
+//    
+//    cs540::Array<short, 1> c = a;
+//    
+//    std::tuple<int, char> foo (10, 'x');
+//    
+//    
+//    
+//    
+//    
+//    
+//    cs540::Array<int, 1, 1> a1, b1;
+//    
+//    auto i1 = a1.fmbegin();
+//    auto i2 = a1.fmend();
+//    
+//    
+//    if (i1 != i2)
+//        std::cout<<*i1<<std::endl;
+//    
+//    i1++;
+//    
+//    if (i1 != i2)
+//        std::cout<<*i1<<std::endl;
+//   
+//    a1[0][0] = 1;
+//    std::cout<<*i1<<std::endl;
+//    a1[0][0] = 2;
+//    std::cout<<*i1<<std::endl;
+//    *i1 = 3;
+//    std::cout<<*i1<<std::endl;
+//    
+//    for(i1 = a1.fmbegin(); i1 != a1.fmend(); i1++)
+//        std::cout<<*i1<<std::endl;
     
-    a[0] = 1;
-    b[0] = a[0];
-    
-    cs540::Array<short, 1> c = a;
-    
-    
-    
-    //cs540::Array<int, 1, 1> a1, b1;
-   
-    //a1[0][0] = 1;
-    //a1[0][0] = 2;
     //b1[0][0] = a1[0][0];
     
     // Define a 2X3X4 array of integers.  Elements are uninitialized.
-    //cs540::Array<int, 2, 3, 4> a, b;
-    //cs540::Array<short, 2, 3, 4> c;
-    //cs540::Array<short, 1, 3, 4> d;
+    cs540::Array<int, 2, 3, 4> a, b;
+    cs540::Array<short, 2, 3, 4> c;
+    cs540::Array<short, 1, 3, 4> d;
     //cs540::Array<int, 0> e1; // This line must cause a compile-time error.
     //cs540::Array<int, 1, 0, 1> e2; // This line must cause a compile-time error.
     //cs540::Array<int, 0, 1, 1> e3; // This line must cause a compile-time error.
     
 //    // Range-checked indexing.
-    //a[0][0][0] = 1234;
-    //a[1][1][1] = a[0][0][0];
-    //a[0][2][3] = 5678; // Set the element in the first plane, 3rd row, and 4th column.
+    a[0][0][0] = 1234;
+    a[1][1][1] = a[0][0][0];
+    a[0][2][3] = 5678; // Set the element in the first plane, 3rd row, and 4th column.
 //    a[0][3][0] = 1; // Out of range, throws.
 //    
     
-    //cs540::Array<int, 2, 3, 4> a2 = a;
-    //cs540::Array<short, 2, 3, 4> c2 = a;
-    //a = a; // Self-assignment must be a no-op.
-    //b = a; // Okay, as long as dimensions and type match exactly.
-//    a = c; // Member template constructor.
-//    
-//    const Array<int, 2, 3, 4> &const_ref(a);
-//    
-//    int i = const_ref[0][0][0]; // Okay.
-//    const_ref[0][0][0] = 1; // Syntax error.
-//    
-//    // Iterate through in plane major order.  (Note: Normally, you would use
-//    // auto to simplify.)
-//    for (Array<3, int>::FirstDimensionMajorIterator it = a.fmbegin(); it != a.fmend(); ++it) {
-//        cout << *it << endl;
-//    }
-//    
-//    // Iterate through in column major order.
-//    for (Array<3, int>::LastDimensionMajorIterator it = a.lmbegin(); it != a.lmend(); ++it) {
-//        cout << *it << endl;
-//    }
+    auto i1 = a.fmbegin();
+    auto i2 = a.fmend();
+    
+    for(int i=0;i1!=i2;++i1, i++)
+        *i1 = i;
+    
+    auto i3 = b.lmbegin();
+    auto i4 = b.lmend();
+    
+    for(int i=0;i3!=i4;++i3, i++)
+        *i3 = i;
+    
+    
+    cs540::Array<int, 2, 3, 4> a2 = a;
+    cs540::Array<short, 2, 3, 4> c2 = a;
+    a = c2;
+    
+    a2 = a;
+    a = a; // Self-assignment must be a no-op.
+    b = a; // Okay, as long as dimensions and type match exactly.
+    a = c; // Member template constructor.
+    
+    const cs540::Array<int, 2, 3, 4> &const_ref(a);
+    
+    a[0][0][0] = 69;
+    int i = const_ref[0][0][0]; // Okay.
+    //const_ref[0][0][0] = 1; // Syntax error.
+    
+    i1 = a.fmbegin();
+    i2 = a.fmend();
+    for(int i=0;i1!=i2;++i1, i++)
+        *i1 = i;
+
+    // Iterate through in plane major order.  (Note: Normally, you would use
+    // auto to simplify.)
+    for (cs540::Array<int, 2, 3, 4>::FirstDimensionMajorIterator it = a.fmbegin(); it != a.fmend(); ++it) {
+        std::cout << *it << std::endl;
+    }
+    
+    // Iterate through in column major order.
+    for (cs540::Array<int, 2, 3, 4>::LastDimensionMajorIterator it = a.lmbegin(); it != a.lmend(); ++it) {
+        std::cout << *it << std::endl;
+    }
 
 }
