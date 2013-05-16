290e16853b9e1221b054248bcda0ec71e40c418e
James <james.kizer@gmail.com>
2013-05-16 08:42:20 -0400

    Development updates to NewThread. To Do:
    Replace ((*object).*ptm)(std::get<0>(argsTuple), std::get<1>(argsTuple), std::get<2>(argsTuple)); with generic invocation.
    
    Create and launch thread.

--- NewThread.hpp
+++ NewThread.hpp
@@ -10,14 +10,68 @@
 #define __CS540project3__NewThread__
 
 #include <iostream>
+#include <assert.h>
+
+namespace cs540 {
+    
+template <typename T, typename R, typename... ATs>
+class Launcher {
+    
+public:
+    
+    Launcher(T *obj, R (T::*mem)(ATs...), ATs... args) : object(obj), ptm(mem),
+                                        argsTuple(std::make_tuple(args...)) {}
+    
+    void entry(void *o) {
+        assert(o == this);
+        
+        ((*object).*ptm)(std::get<0>(argsTuple), std::get<1>(argsTuple), std::get<2>(argsTuple));
+        //(*object).*ptm();
+    }
+    
+    T *object;
+    R (T::*ptm)(ATs...);
+    std::tuple<ATs...> argsTuple;
+};
+    
+template <typename T, typename R>
+class Launcher<T,R> {
+        
+    public:
+        
+        Launcher(T *obj, R (T::*mem)()) : object(obj), ptm(mem) {}
+        
+        void entry(void *o) {
+            assert(o == this);
+            
+            //(*object).*ptm(std::get<0>(argsTuple), std::get<1>(argsTuple), std::get<2>(argsTuple));
+            ((*object).*ptm)();
+        }
+        
+        T *object;
+        R (T::*ptm)();
+    };
+template <typename T, typename R>
+    pthread_t NewThread( T *obj, R (T::*mem)() ) {
+        Launcher<T, R> l(obj, mem);
+        
+        l.entry(&l);
+    }
 
 template <typename T, typename R, typename... ATs>
-pthread_t NewThread(T *obj, R (T::*mem)(ATs...), ATs... args);
+pthread_t NewThread(T *obj, R (T::*mem)(ATs...), ATs... args) {
+    Launcher<T, R, ATs...> l(obj, mem, args...);
+    
+    l.entry(&l);
+}
+    
+
 
 // 1) need to generate a thread entry point that invokes obj->mem(args...)
 // 2) can we create a class that contains an object, member function pointer and another object to contain the variadic arguments?
 // 2a) is there a way to store args such that it can be reproduced and used as a argument pack again?
 // 3) try std::tuple
+//  can use std::tie (args...) = tuple?
 
 //template <typename T, typename R, typename... ATs>
 //void entry<T,R,ATs...>(){
@@ -25,5 +79,7 @@ pthread_t NewThread(T *obj, R (T::*mem)(ATs...), ATs... args);
 //}
 
 //obj->mem(args...);
+    
+}
 
 #endif /* defined(__CS540project3__NewThread__) */
--- NewThreadTest.cpp
+++ NewThreadTest.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <string>
 #include "NewThread.hpp"
 
 void test(int i, double d, bool b){
@@ -17,11 +18,56 @@ void (*testfunc)(int, double, bool) = &test;
 
 template <typename... ATs>
 void TemplateTest(ATs... args){
-    testfunc(args...);
+    //argsTuple
+    
+    std::tuple<ATs...> argsTuple(std::make_tuple(args...));
+    //testfunc(args...);
 }
+
+
+class LauncherTestClass {
+public:
+    LauncherTestClass(int i_, double j_, std::string k_):i(i_), j(j_), k(k_){}
+    
+    void function(int x, double y, std::string z) {
+        i=x;
+        j=y;
+        k=z;
+    }
+    
+    int i;
+    double j;
+    std::string k;
+};
+
+class EmptyLauncherTestClass {
+public:
+    void function(){
+        
+    }
+};
+
 
 int main() {
     
-    TemplateTest(1, 1.0, true);
+    //TemplateTest(1, 1.0, true);
+    std::string s("This is my initial test string");
+    LauncherTestClass testObj(1, 2.2, s);
+    s = "This is my updated string";
+    //ptm
+    void (LauncherTestClass::* ptmf) (int, double, std::string) = &LauncherTestClass::function;
+    
+    //TemplateTest(3, 4.5, s);
+    
+    
+    
+    pthread_t ret = cs540::NewThread<LauncherTestClass, void, int, double, std::string> (&testObj, ptmf, 3, 4.5, s);
+    
+    //EmptyLauncherTestClass l1;
+    //void (EmptyLauncherTestClass::* lptmf) () = &EmptyLauncherTestClass::function;
+
+    
+    //pthread_t ret = cs540::NewThread<EmptyLauncherTestClass, void>( &l1, lptmf);
+    
     
 }
